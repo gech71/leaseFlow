d8bb59c6d1d1f703c7ffcd4ea986327fd4544fe4
Robel Asaminew <robelas2001@gmail.com>
2025-06-21 07:45:14 +0000

    change only the interface to include all the missing permissions dont ad

--- src/app/admin/settings/role-management/client-page.tsx
+++ src/app/admin/settings/role-management/client-page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import React, { useState, useEffect } from 'react';
@@ -368,3 +367,5 @@ export function RoleManagementClientPage({ initialRoles }: RoleManagementClientP
     </Card>
   );
 }
+
+    
