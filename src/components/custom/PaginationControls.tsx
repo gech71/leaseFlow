50524cdd0eae7cbb7efe74132e9f7ebd41f8a579
Robel Asaminew <robelas2001@gmail.com>
2025-06-30 08:25:31 +0000

    in every row per page remove 8 and 9 start from 5 and increment with 5

--- src/components/custom/PaginationControls.tsx
+++ src/components/custom/PaginationControls.tsx
@@ -88,7 +88,7 @@ export function PaginationControls({
                 <SelectValue placeholder={String(itemsPerPage)} />
               </SelectTrigger>
               <SelectContent>
-                {[5, 8, 9, 10, 15, 20, 50].map(size => (
+                {[5, 10, 15, 20, 25, 50].map(size => (
                   <SelectItem key={size} value={String(size)}>{size}</SelectItem>
                 ))}
               </SelectContent>
