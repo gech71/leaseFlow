2b86fa388147105ea6490254ce61dde867618802
Robel Asaminew <robelas2001@gmail.com>
2025-08-29 08:33:03 +0000

    Error: useState is not defined
    src\app\admin\settings\email-configurati

--- src/app/admin/settings/email-configuration/client-page.tsx
+++ src/app/admin/settings/email-configuration/client-page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import React, from 'react';
+import React, { useState } from 'react';
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
