40bcd9d9fff0c1d72113282c188daf87e04862fa
Robel Asaminew <robelas2001@gmail.com>
2025-09-01 12:36:58 +0000

    rename Template Name placeholder to Template Name

--- src/app/admin/settings/agreement-template/upsert/form.tsx
+++ src/app/admin/settings/agreement-template/upsert/form.tsx
@@ -178,7 +178,7 @@ export function UpsertTemplateForm({ initialData }: UpsertTemplateFormProps) {
                             <FormItem>
                                 <FormLabel>Template Name</FormLabel>
                                 <FormControl>
-                                <Input placeholder="e.g., Standard Residential Lease" {...field} />
+                                <Input placeholder="Template Name" {...field} />
                                 </FormControl>
                                 <FormMessage />
                             </FormItem>
