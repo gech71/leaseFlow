6dbd247a7577d2bf7b1bfd77543a7c1ea0afdf90
Robel Asaminew <robelas2001@gmail.com>
2025-09-01 11:14:01 +0000

    remove duplicated and unwanted files

--- src/app/actions.ts
+++
@@ -1,9 +0,0 @@
-
-// src/app/actions.ts
-"use server";
-
-// Agreement generator flow removed as it's now handled client-side.
-// Bill analyzer related imports removed
-
-// All actions have been moved to their respective feature folders (e.g., src/app/admin/agreements/actions.ts)
-// This file can be removed if no global server actions are needed.
