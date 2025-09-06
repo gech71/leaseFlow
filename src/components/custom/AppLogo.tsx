90f8ae56902bc97ba0e35f7bef6a96d7d50bc559
Robel Asaminew <robelas2001@gmail.com>
2025-09-06 08:32:45 +0000

    rename nibrenatl to Building Management Solution on the side bar

--- src/components/custom/AppLogo.tsx
+++ src/components/custom/AppLogo.tsx
@@ -14,7 +14,7 @@ export function AppLogo() {
     <Link href="/admin/dashboard" className="flex items-center gap-2 text-sidebar-foreground hover:text-sidebar-accent-foreground transition-colors w-full">
       <Image 
         src="https://i.imgur.com/JTzGpIH.png"
-        alt="nibrental Logo" 
+        alt="Building Management Solution Logo" 
         width={40}
         height={40}
         className={cn(
@@ -29,7 +29,7 @@ export function AppLogo() {
         <h1 className={cn(
           "font-headline text-base leading-tight font-bold text-sidebar-primary",
         )}>
-          nibrental
+          Building Management Solution
         </h1>
       </div>
     </Link>
