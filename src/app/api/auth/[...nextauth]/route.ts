aa7d3b5994f8183c95e5530419a469cbde89fc99
Robel Asaminew <robelas2001@gmail.com>
2025-11-09 17:48:55 +0000

    please update the [...nestAuth] file to use the latest v5 nextauth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,2 @@
-import { handlers } from "@/auth" // Referring to the new auth.ts file
-export const { GET, POST } = handlers
+import { handlers } from "@/auth";
+export const { GET, POST } = handlers;
