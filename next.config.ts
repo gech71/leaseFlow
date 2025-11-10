340ad17b2f102ca14f5ed44c63b493ac6aa2a87d
Robel Asaminew <robelas2001@gmail.com>
2025-11-10 20:27:48 +0000

    include the following on the nextcomfig file
      async headers() {
        re

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,22 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "X-Frame-Options", value: "SAMEORIGIN" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
