e0f30b5e64268ee13288ca5ec3f6687a898cabc6
Robel Asaminew <robelas2001@gmail.com>
2025-11-11 08:20:37 +0000

    A tree hydrated but some attributes of the server rendered HTML didn't m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,22 +21,19 @@ export default async function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="https://i.imgur.com/JTzGpIH.png" type="image/png" sizes="any" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" nonce={nonce} />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
-          nonce={nonce}
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
-          nonce={nonce}
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=swap"
           rel="stylesheet"
-          nonce={nonce}
         />
       </head>
       <body className="font-body antialiased">
